7a53840c687292f9a312b8cd9a7c0cfd5e26744b
Krish-Das <krishdasatgames@gmail.com>
2025-09-29 17:08:22 +0530

    solve: 28 Find occurence in a string

---
+++ 28-index-of-first-occurrence-in-string/sol-1.ts
@@ -0,0 +1,8 @@
+function strStr(haystack: string, needle: string): number {
+  if (!haystack.includes(needle)) return -1;
+  return haystack.indexOf(needle);
+}
+
+console.clear();
+console.log(strStr("sadbutsad", "sad"));
+console.log(strStr("leetcode", "leeto"));
