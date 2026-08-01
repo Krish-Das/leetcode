6bcb8dc3a3f7d48d99c47ca89b8209aeac19e88d
Krish-Das <krishdasatgames@gmail.com>
2026-08-01 12:51:54 +0530

    solve: 49. Group Anagrams

---
+++ 49-group-anagrams/index.ts
@@ -0,0 +1,18 @@
+function groupAnagrams(strs: string[]): string[][] {
+  const map = new Map<string, string[]>();
+  for (const str of strs) {
+    const groupName = str.split("").sort().join("");
+    const group = map.get(groupName);
+
+    if (!group) {
+      map.set(groupName, [str]);
+      continue;
+    }
+
+    map.set(groupName, [...group, str]);
+  }
+
+  return [...map.values()];
+}
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
