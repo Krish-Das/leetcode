226bffe85ec1970e46503cbc8d0d7a0f082feca4
Krish-Das <krishdasatgames@gmail.com>
2026-08-04 15:42:40 +0000

    refactor: add bucket sort solution
    - Implement bucket sort approach as alternative to heap-based solution
    - Rename original implementation to _topKFrequent for reference
    - Use frequency buckets to achieve O(n) time complexity
    - Uncomment and fix test cases

--- 347-top-k-frequent-elements/index.ts
+++ 347-top-k-frequent-elements/index.ts
@@ -1,4 +1,4 @@
-function topKFrequent(nums: number[], k: number): number[] {
+function _topKFrequent(nums: number[], k: number): number[] {
   const map = new Map<number, number[]>();
 
   for (const num of nums) {
@@ -19,9 +19,29 @@ function topKFrequent(nums: number[], k: number): number[] {
   return topK.map((a) => a.at(0)!);
 }
 
+function topKFrequent(nums: number[], k: number): number[] {
+  const map = new Map<number, number>();
+
+  for (const num of nums) {
+    map.set(num, (map.get(num) ?? 0) + 1);
+  }
+
+  // Array pre allocation in JS for some reason!
+  const buckets: number[][] = Array.from({ length: nums.length + 1 }, () => []);
+  for (const [num, count] of map) {
+    buckets[count]!.push(num);
+  }
+
+  const result: number[] = [];
+  for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+    result.push(...buckets[i]!);
+  }
+  return result.slice(0, k);
+}
+
 console.log(`ans: ${topKFrequent([1, 1, 1, 2, 2, 3], 2)}`); // [1, 2]
 console.log(`ans: ${topKFrequent([1], 1)}`); // [1]
 console.log(`ans: ${topKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2)}`); // [1, 2]
 
-// console.log(topKFrequent([1, 2, 3, 2, 3, 2, 3, 1, 3, 2], 2)); // [1, 2]
-// console.log(`ans: ${topKFrequent([2, 2, 1, 1, 2], 1)}`);
+topKFrequent([1, 2, 3, 2, 3, 2, 3, 1, 3, 2], 3); // [1, 2]
+console.log(`ans: ${topKFrequent([2, 2, 1, 1, 2], 1)}`);
