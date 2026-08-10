213438e36dccb311ea6addcb9b8fdde640dd80b4
Krish-Das <krishdasatgames@gmail.com>
2026-08-10 14:06:42 +0000

    solve(leetcode): add product of array except self solution
    Implement two approaches: a naive division-based method (wip) and a
    memoized solution that caches products for duplicate values. The
    memoized version trades space for time when the array contains
    repeated elements.

---
+++ src/leetcode/0238-product-of-array-except-self/index.ts
@@ -0,0 +1,54 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: nums will always be in the range */
+
+/**
+ * @todo breaks when nums contains 0
+ */
+function _productExceptSelf(nums: number[]): number[] {
+  const productOfAll = nums.reduce((acc, num) => acc * num, 1)
+  const bucket: number[] = []
+  for (const num of nums) {
+    const productExceptNum = productOfAll / num
+    bucket.push(productExceptNum)
+  }
+  return bucket
+}
+
+// worst: O(n²); best: O(n)
+function productExceptSelf(nums: number[]): number[] {
+  const map = new Map<number, number>()
+  const bucket: number[] = []
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]!
+    const cache = map.get(num)
+    // cache hit
+    if (cache !== undefined) {
+      bucket.push(cache)
+      continue
+    }
+
+    // cache miss
+    let product = 1
+    for (let j = 0; j < nums.length; j++) {
+      if (j === i) continue
+      product *= nums[j]!
+    }
+
+    // update cache
+    map.set(num, product)
+    bucket.push(product)
+  }
+
+  return bucket
+}
+
+console.log(
+  productExceptSelf([1, 2, 3, 4]), // [24,12,8,6]
+)
+
+console.log(
+  productExceptSelf([-1, 1, 0, -3, 3]), // [0,0,9,0,0]
+)
+
+console.log(
+  productExceptSelf([0, 0]), // [0,0,9,0,0]
+)
