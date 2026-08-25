9811bbb96a1fd28a19cf0125099bc8a4797b6bc4
Krish-Das <krishdasatgames@gmail.com>
2026-08-25 16:58:31 +0000

    fix(leetcode): add solution to 875. koko eating bananas

--- src/leetcode/0875-koko-eating-bananas/binary-search.ts
+++ src/leetcode/0875-koko-eating-bananas/binary-search.ts
@@ -1,5 +1,18 @@
 function minEatingSpeed(piles: number[], h: number): number {
-  return 0 as never
+  let ans = Math.max(...piles)
+  let l = 1
+  let r = ans
+
+  while (l <= r) {
+    const m = Math.floor((l + r) / 2)
+    let hour = 0
+    for (const p of piles) hour += Math.ceil(p / m)
+    if (hour <= h) {
+      ans = Math.min(ans, m)
+      r = m - 1
+    } else l = m + 1
+  }
+  return ans
 }
 
 console.log(minEatingSpeed([3, 6, 7, 11], 8))
