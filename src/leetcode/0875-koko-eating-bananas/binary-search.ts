a2158f73a294454aa4af9f17020379df97352e59
Krish-Das <krishdasatgames@gmail.com>
2026-08-25 15:23:38 +0000

    leetcode: solve 875. koko eating bananas

---
+++ src/leetcode/0875-koko-eating-bananas/binary-search.ts
@@ -0,0 +1,7 @@
+function minEatingSpeed(piles: number[], h: number): number {
+  return 0 as never
+}
+
+console.log(minEatingSpeed([3, 6, 7, 11], 8))
+console.log(minEatingSpeed([30, 11, 23, 4, 20], 5))
+console.log(minEatingSpeed([30, 11, 23, 4, 20], 6))
