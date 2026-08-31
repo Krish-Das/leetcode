b69d51d7b02eaee5c271486dd161fe2cb304209f
Krish-Das <krishdasatgames@gmail.com>
2026-08-31 16:40:49 +0000

    leetcode: resolve & refactor 238. Product of Array Except Self
    Optimize algorithm to run in O(1) space complexity

--- src/leetcode/0238-product-of-array-except-self/prefix-suffix-products.ts
+++ src/leetcode/0238-product-of-array-except-self/prefix-suffix-products.ts
@@ -1,37 +1,21 @@
 function productExceptSelf(nums: number[]): number[] {
-  const pre: number[] = []
-  let preAcc: number = 1
-  for (const num of nums) {
-    preAcc *= num
-    pre.push(preAcc)
+  const ans = Array.from({ length: nums.length }, () => 1)
+
+  // Prefix
+  for (let i = 0, acc = 1; i < nums.length - 1; i++) {
+    // biome-ignore lint/style/noNonNullAssertion: 0 < i < len-1
+    acc *= nums[i]!
+    ans[i + 1] = acc
   }
 
-  const post: number[] = []
-  let postAcc: number = 1
-  for (let i = nums.length - 1; i >= 0; i--) {
-    // biome-ignore lint/style/noNonNullAssertion: nums[i] is never out of bound
-    const num = nums[i]!
-    postAcc *= num
-    post[i] = postAcc
+  // Postfix + ans
+  for (let i = nums.length - 1, acc = 1; i >= 0; i--) {
+    ans[i] = (ans[i] ?? 1) * acc
+    // biome-ignore lint/style/noNonNullAssertion: len-1 >= i >= 0
+    acc *= nums[i]!
   }
 
-  const bucket: number[] = []
-  for (let i = 0; i < nums.length; i++) {
-    const preNum = pre[i - 1] ?? 1
-    const postNum = post[i + 1] ?? 1
-    const product = preNum * postNum
-    bucket.push(product)
-  }
-
-  return bucket
+  return ans
 }
 
-console.log(
-  productExceptSelf([1, 2, 3, 4]), // [24,12,8,6]
-)
-console.log(
-  productExceptSelf([-1, 1, 0, -3, 3]), // [0,0,9,0,0]
-)
-console.log(
-  productExceptSelf([0, 0]), // [0,0,9,0,0]
-)
+export { productExceptSelf }
