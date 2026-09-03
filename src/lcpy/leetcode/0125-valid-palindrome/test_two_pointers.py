cbf9b7fd62f6389bf6346ac66e075f833e8c18dc
Krish-Das <krishdasatgames@gmail.com>
2026-09-03 09:07:39 +0000

    style: add return type annotations to test functions

--- src/lcpy/leetcode/0125-valid-palindrome/test_two_pointers.py
+++ src/lcpy/leetcode/0125-valid-palindrome/test_two_pointers.py
@@ -1,16 +1,16 @@
 from two_pointers import Solution
 
 
-def test_valid_palindrome():
+def test_valid_palindrome() -> None:
     sol = Solution()
     assert sol.isPalindrome("A man, a plan, a canal: Panama") is True
 
 
-def test_not_palindrome():
+def test_not_palindrome() -> None:
     sol = Solution()
     assert sol.isPalindrome("race a car") is False
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     sol = Solution()
     assert sol.isPalindrome(" ") is True
