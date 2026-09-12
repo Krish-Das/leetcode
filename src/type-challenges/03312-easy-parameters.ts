ad5c57bf66c321721b3bf537b0a622cdcec23899
Krish-Das <krish@upsher.dev>
2026-09-12 23:14:56 +0530

    solve: Type Challenges 3312 (Parameters)
    Refs: https://github.com/type-challenges/type-challenges/blob/main/questions/03312-easy-parameters

---
+++ src/type-challenges/03312-easy-parameters.ts
@@ -0,0 +1,15 @@
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type MyParameters<T> = T extends (...args: infer U) => unknown ? U : never
+
+// ================== TEST CASES ==================
+
+function foo(_arg1: string, _arg2: number): void {}
+function bar(_arg1: boolean, _arg2: { a: "A" }): void {}
+function baz(): void {}
+
+type _cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: "A" }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+]
