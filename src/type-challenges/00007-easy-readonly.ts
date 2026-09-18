43e93118ddd79904791a515791648253f28e7f77
Krish-Das <krish@upsher.dev>
2026-09-18 13:13:43 +0530

    test(tc-7): add more test cases for Readonly

--- src/type-challenges/00007-easy-readonly.ts
+++ src/type-challenges/00007-easy-readonly.ts
@@ -1,6 +1,10 @@
+import type { Equal, Expect } from "@type-challenges/utils"
+
 type MyReadonly<T> = {
   readonly [P in keyof T]: T[P]
 }
+
+// ================== TEST CASES ==================
 
 interface Todo {
   title: string
@@ -16,3 +20,14 @@ const todo: MyReadonly<Todo> = {
 todo.title = "Hello"
 // @ts-expect-error: cannot reassign a readonly property
 todo.description = "barFoo"
+
+type _cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
