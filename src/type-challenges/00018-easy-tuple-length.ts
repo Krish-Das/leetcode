332fe979035dcc75b7cff2517c9361d74cbe6d10
Krish-Das <krish@upsher.dev>
2026-09-18 14:48:27 +0530

    test(tc-18): tighten test cases for Tuple Length

--- src/type-challenges/00018-easy-tuple-length.ts
+++ src/type-challenges/00018-easy-tuple-length.ts
@@ -2,16 +2,22 @@ import type { Equal, Expect } from "@type-challenges/utils"
 
 type Length<T extends readonly unknown[]> = T["length"]
 
-type tesla = ["tesla", "model 3", "model X", "model Y"]
-type spaceX = [
+// ================== TEST CASES ==================
+
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const
+const spaceX = [
   "FALCON 9",
   "FALCON HEAVY",
   "DRAGON",
   "STARSHIP",
   "HUMAN SPACEFLIGHT",
+] as const
+
+type _cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<"hello world">,
 ]
-
-type teslaLength = Length<tesla>
-type spaceXLength = Length<spaceX>
-
-type _cases = [Expect<Equal<teslaLength, 4>>, Expect<Equal<spaceXLength, 5>>]
