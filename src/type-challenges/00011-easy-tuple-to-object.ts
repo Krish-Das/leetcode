1afae9c4f773fa0b3d0773588a62d88c7047bc4d
Krish-Das <krish@upsher.dev>
2026-09-18 14:21:18 +0530

    test(tc-11): add more test cases for Tuple to Object

--- src/type-challenges/00011-easy-tuple-to-object.ts
+++ src/type-challenges/00011-easy-tuple-to-object.ts
@@ -4,17 +4,42 @@ type TupleToObject<T extends readonly PropertyKey[]> = {
   [P in T[number]]: P
 }
 
-const tuple = ["tesla", "model 3", "model X", "model Y"] as const
-type result = TupleToObject<typeof tuple>
+// ================== TEST CASES ==================
 
-type _cases = Expect<
-  Equal<
-    result,
-    {
-      tesla: "tesla"
-      "model 3": "model 3"
-      "model X": "model X"
-      "model Y": "model Y"
-    }
-  >
->
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const
+const tupleNumber = [1, 2, 3, 4] as const
+const sym1 = Symbol(1)
+const sym2 = Symbol(2)
+const tupleSymbol = [sym1, sym2] as const
+const tupleMix = [1, "2", 3, "4", sym1] as const
+
+type _cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla"
+        "model 3": "model 3"
+        "model X": "model X"
+        "model Y": "model Y"
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleSymbol>,
+      { [sym1]: typeof sym1; [sym2]: typeof sym2 }
+    >
+  >,
+  Expect<
+    Equal<
+      TupleToObject<typeof tupleMix>,
+      { 1: 1; "2": "2"; 3: 3; "4": "4"; [sym1]: typeof sym1 }
+    >
+  >,
+]
+
+// @ts-expect-error
+// biome-ignore lint/complexity/noBannedTypes: test case
+type _error = TupleToObject<[[1, 2], {}]>
