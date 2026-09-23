3955d70baf6f5110216bf73e9a47fa8cba176c71
Krish-Das <krish@upsher.dev>
2026-09-23 21:11:13 +0530

    solve: Type Challenges 16 (Pop)
    Refs: https://github.com/type-challenges/type-challenges/blob/main/questions/00016-medium-pop

---
+++ src/type-challenges/00016-medium-pop.ts
@@ -0,0 +1,16 @@
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type _cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<["a", "b", "c", "d"]>, ["a", "b", "c"]>>,
+  Expect<Equal<Pop<[]>, []>>,
+]
+
+// ================== SOLUTION ==================
+
+type Pop<Tupple extends readonly unknown[]> = Tupple extends readonly [
+  ...infer Rest,
+  unknown,
+]
+  ? Rest
+  : []
