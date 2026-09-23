b2b4b2c350a195dd897055c3a2e2b78a95bdbb13
Krish-Das <krish@upsher.dev>
2026-09-23 23:13:10 +0530

    solve: Type Challenges 62 (Type Lookup)
    Refs: https://github.com/type-challenges/type-challenges/blob/main/questions/00062-medium-type-lookup

---
+++ src/type-challenges/00062-medium-type-lookup.ts
@@ -0,0 +1,23 @@
+import type { Equal, Expect } from "@type-challenges/utils"
+
+interface Cat {
+  type: "cat"
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal"
+}
+
+interface Dog {
+  type: "dog"
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer"
+  color: "brown" | "white" | "black"
+}
+
+type Animal = Cat | Dog
+
+type _cases = [
+  Expect<Equal<LookUp<Animal, "dog">, Dog>>,
+  Expect<Equal<LookUp<Animal, "cat">, Cat>>,
+]
+
+// ================== SOLUTION ==================
+
+type LookUp<U, T> = U extends { type: T } ? U : never
